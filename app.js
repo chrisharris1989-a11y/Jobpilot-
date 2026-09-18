5586fed64497786bc85d5261c954ddd9f0163803
chrisharris1989-a11y <development@jobpilotcrm.co.uk>
2026-09-18 12:36:25 +0100

    Restore stable app startup

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { startSignup } from "./signup-flow.js";
 import { showFeedbackForm } from "./feedback.js";
 import { showFeedbackAdmin } from "./feedback-admin.js";
 import { getJobPilotAddressContext, normalizeJobPilotPostalCode } from "./regional-address.js";
-import "./ui/user-permission-gate.js";
 
 let currentUser = null;
 let customers = [];

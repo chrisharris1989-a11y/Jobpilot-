4ed6c0222fba15b59c029bd79a1b8cb0e1f2bdd0
chrisharris1989-a11y <development@jobpilotcrm.co.uk>
2026-09-18 13:54:58 +0100

    perf: reuse cached auth user lookup

--- ui/quote-terms-autofill.js
+++ ui/quote-terms-autofill.js
@@ -1,5 +1,5 @@
 import "./app-preferences.js";
-import { supabase } from "../supabase.js";
+import { supabase, getCachedUserResponse } from "../supabase.js";
 
 (() => {
   let templatePromise = null;
@@ -8,7 +8,7 @@ import { supabase } from "../supabase.js";
     if (templatePromise) return templatePromise;
 
     templatePromise = (async () => {
-      const { data: { user } = {} } = await supabase.auth.getUser();
+      const { data: { user } = {} } = await getCachedUserResponse();
       if (!user) return "";
 
       let companyId = null;
